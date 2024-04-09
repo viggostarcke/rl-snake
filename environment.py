b05b5662732860cc129a2257f4d9b40d4ffc42a1
zikkid <viggost4rcke@gmail.com>
2024-04-10 01:19:09 +0200

    FIX: reward function tweaks ADD: comments for every function

--- environment.py
+++ environment.py
@@ -42,6 +42,14 @@ class SnakeEnv(gym.Env):
         # BOX: distance compass of tuples: (dist_to_obstacle, dist_to_apple)
         self.observation_space = spaces.Box(low=0, high=1, shape=(8, 2), dtype=np.float32)
 
+        # DICT: distance compass of tuples : (dist_to_obstacle, dist_to_apple) & 3 adjacent tiles
+        self.observation_space = spaces.Dict(
+            {
+                'compass_distances': spaces.Box(low=0, high=1, shape=(8, 2), dtype=np.float32),
+                'adjacent_tiles': spaces.Box(low=0, high=1, shape=(3,), dtype=np.float32)
+            }
+        )
+
         # 0: left, 1: continue, 2: right
         self._action_to_dir = {
             'left': {
@@ -69,6 +77,7 @@ class SnakeEnv(gym.Env):
         self.window = None
         self.clock = None
 
+    # DICT: apple compass dir & 3 adjacent tiles
     # def _get_observation(self):
     #     apple_dir = self.get_apple_dir(self.apple_coord)
     #     corrected_apple_dir = self.rotate_apple_dir(apple_dir)
@@ -79,21 +88,38 @@ class SnakeEnv(gym.Env):
     #         "adjacent_tiles": adjacent_tiles
     #     }
 
+    # BOX: distance compass of tuples: (dist_to_obstacle, dist_to_apple)
+    # def _get_observation(self):
+    #     vision_rays = self.rotate_vision_rays(self.get_vision_rays())
+    #     norm_vision_rays = np.copy(vision_rays).astype(np.float32)
+    #     norm_vision_rays[:, 0] = norm_vision_rays[:, 0] / 10
+    #     norm_vision_rays[:, 1] = norm_vision_rays[:, 1] / 10
+    #
+    #     return np.array(norm_vision_rays)
+
     def _get_observation(self):
+        # DICT: distance compass of tuples : (dist_to_obstacle, dist_to_apple) & 3 adjacent tiles
+
         vision_rays = self.rotate_vision_rays(self.get_vision_rays())
         norm_vision_rays = np.copy(vision_rays).astype(np.float32)
         norm_vision_rays[:, 0] = norm_vision_rays[:, 0] / 10
-        norm_vision_rays[:, 1] = norm_vision_rays[:, 1] / 10
+        norm_vision_rays[:, 1] = np.array(norm_vision_rays[:, 1] / 10)
 
-        return np.array(norm_vision_rays)
+        adjacent_tiles = np.array(self.get_adjacent_tiles())
+
+        return {
+            "compass_distances": norm_vision_rays,
+            "adjacent_tiles": adjacent_tiles
+        }
 
     def _get_info(self):
         return {
             "episode": {"r": self.score, "l": self.curr_episode_length},
         }
 
-    # reset snake to mid of screen, apples current coordinates and score
     def reset(self, seed=None, options=None):
+        # reset snake and apple's position, and score
+
         self.snake.reset(self.board_dim)
         self.score = 0
         self.reset_apple()
@@ -108,34 +134,36 @@ class SnakeEnv(gym.Env):
         return observation, info
 
     def step(self, action):
+        # moves the snake according to given action, then checks for collisions with either obstacles or apple; and rewards accordingly.
+
         # 3 action space
         curr_dir = self.snake.get_dir()
         new_dir = self._action_to_dir[curr_dir][int(action)]
-        # 4 action space
+        # 4 action space: retired
         # action_dir_map = {0: 'left', 1: 'right', 2: 'up', 3: 'down'}
         # new_dir = action_dir_map[int(action)]
 
-        # move snake body, then head
+        # move snake's body, then head
         self.snake.move()
         curr_head_pos = self.snake.get_head()
         self.move_head(new_dir, curr_head_pos)
+
         done = False
         reward = 0
 
         if self.snake.check_wall_collision(self.board_dim) or self.snake.check_self_collision():
-            reward -= 100
+            reward -= 1000
             self.reset_apple()
             done = True
         elif self.hunger >= self.stamina:
-            reward -= 100
+            reward -= 200
             done = True
         else:
             if self.snake.check_apple_eat(self.apple_coord):
                 self.snake.grow()
                 self.score += 1
-                manhattan_dist = self.get_manhattan_dist(self.last_apple_coord, self.apple_coord)
                 self.last_apple_coord = self.apple_coord
-                reward += int(manhattan_dist) * 20
+                reward += 300
                 self.reset_apple()
                 self.hunger = 0
             else:
@@ -156,8 +184,9 @@ class SnakeEnv(gym.Env):
 
         return observation, reward, done, False, info
 
-    # tries to place apple randomly, if apple is not in snake body returns
     def reset_apple(self):
+        # resets apple's position
+
         while True:
             self.apple_coord = tuple(self.np_random.integers(0, self.board_dim, size=2, dtype=int))
             if not self.snake.check_apple_coord(self.apple_coord):
@@ -165,6 +194,8 @@ class SnakeEnv(gym.Env):
 
     #
     def move_head(self, dir, curr_head_pos):
+        # moves head according to given string dir
+
         if dir == 'left':
             self.snake.set_head((curr_head_pos[0] - 1, curr_head_pos[1]))
             self.snake.set_dir('left')
@@ -178,13 +209,16 @@ class SnakeEnv(gym.Env):
             self.snake.set_head((curr_head_pos[0], curr_head_pos[1] + 1))
             self.snake.set_dir('down')
 
-    def get_prev_moves(self):
-        return self.prev_moves
-
     def render(self):
+        # renders the current game state
+
         return self._render_frame()
 
     def _render_frame(self):
+        """
+        updates the display window with current game state.
+        """
+
         if self.window is None and self.render_mode == "human":
             pygame.init()
             size = (self.x_max, self.y_max)
@@ -211,7 +245,6 @@ class SnakeEnv(gym.Env):
         x = self.square_size * self.apple_coord[0]
         y = self.square_size * self.apple_coord[1]
         self.window.blit(apple_image, (x, y))
-        # pygame.draw.rect(self.window, 'red', [x, y, self.square_size, self.square_size])
 
         # draw snake
         arrow_left = pygame.image.load('.img/arrow_left.png')
@@ -250,9 +283,14 @@ class SnakeEnv(gym.Env):
             pygame.display.quit()
             pygame.quit()
 
-    # gets compass direction apple is in relative to snake head
-    # assumes snake direction is 'up'
     def get_apple_dir(self, apple):
+        """
+        gets compass direction apple is in assuming a direction of 'up'.
+
+        :param apple: apple coordinates.
+        :return: an integer indicating which compass direction apple is located in.
+        """
+
         snake_head_x, snake_head_y = self.snake.get_head()
         apple_x, apple_y = apple
 
@@ -284,8 +322,14 @@ class SnakeEnv(gym.Env):
             # apple and snake head same tile
             return 8
 
-    # rotates get_apple_dir to fit snakes direction
     def rotate_apple_dir(self, compass_dir):
+        """
+        rotates get_apple_dir to fit snakes actual direction.
+
+        :param compass_dir: return statement from get_apple_dir, an integer indicating what compass direction apple is located in.
+        :return: rotated compass direction according to snake's direction.
+        """
+
         snake_dir = self.snake.get_dir()
         if snake_dir == 'left':
             return (compass_dir + 6) % 8
@@ -296,9 +340,16 @@ class SnakeEnv(gym.Env):
         return compass_dir
 
     def get_adjacent_tiles(self):
+        """
+        gets 3 immediate surrounding tiles around snake's head, and assigns a value according to content of that tile.
+        wall/body = 1.0, apple = 0.5, nothing = 0.0.
+
+        :return: tuple containing 3 floats describing 3 adjacent tiles relative to snake's head and direction.
+        """
+
         snake_head_pos = np.array(self.snake.get_head())
         snake_head_dir = self.snake.get_dir()
-        tiles = [0, 0, 0]
+        tiles = [0.0, 0.0, 0.0]
         adjacent_tiles = {
             'left': snake_head_pos + np.array([-1, 0]),
             'right': snake_head_pos + np.array([1, 0]),
@@ -311,18 +362,21 @@ class SnakeEnv(gym.Env):
 
         for i, tile in enumerate(adjacent_tiles):
             if tile[0] < 0 or tile[0] == self.board_dim or tile[1] < 0 or tile[1] == self.board_dim:
-                tiles[i] = 1
+                tiles[i] = 1.0
             elif self.snake.check_apple_eat(tile):  # reusing code for apple eat check (i.e. if tile is in body)
-                tiles[i] = 1
+                tiles[i] = 1.0
             elif tile[0] == self.apple_coord[0] and tile[1] == self.apple_coord[1]:
                 tiles[i] = 0.5
 
         return tiles
 
-    def get_manhattan_dist(self, coords1, coords2):
-        return abs(coords1[0] - coords2[0]) + abs(coords1[1] - coords2[1])
-
     def get_vision_rays(self):
+        """
+        gets 8 tuples, 1 for each compass direction, representing distance to nearest obstacle and distance to apple.
+
+        :return: 8x2 element array containing tuples.
+        """
+
         directions = [(0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1)]  # all compass directions clockwise
         vision = np.zeros((8, 2))
 
@@ -332,6 +386,13 @@ class SnakeEnv(gym.Env):
         return vision
 
     def rotate_vision_rays(self, vision_rays):
+        """
+        rotates a given vision ray to fit snake's direction making it a compass of distance tuples.
+
+        :param vision_rays: 8x2 element array containing tuples.
+        :return: 8x2 element array containing tuples. order: start at forward and rotate clockwise
+        """
+
         snake_dir = self.snake.get_dir()
 
         dir_steps = {
@@ -348,6 +409,14 @@ class SnakeEnv(gym.Env):
         return rotated_vision_rays
 
     def calc_distances(self, dx, dy):
+        """
+        calculates distance to nearest obstacle and apple for a coordinate relative to snake's head position.
+
+        :param dx: x coordinate.
+        :param dy: y coordinate.
+        :return: tuple: (dist_to_obstacle, dist_to_apple).
+        """
+
         x, y = self.snake.get_head()
         dist_to_obstacle = None
         dist_to_apple = None
